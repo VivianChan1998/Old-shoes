36edcac0dc3a52e5bb2453842fdd545009f4019a
FAREAST\v-yhsiao <v-yhsiao@microsoft.com>
2020-07-07 11:25:06 +0800

    CV: modified main.py size.py from no-func to functional

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -9,53 +9,55 @@ from io import BytesIO
 #from config import MY_PATH
 import config_own
 
-# Set the COMPUTER_SUBSCRIPTION_KEY & COMPUTER_VISION_ENDPOINT
-if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
-    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
-else:
-    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
+def img_processing():
+    # Set the COMPUTER_SUBSCRIPTION_KEY & COMPUTER_VISION_ENDPOINT
+    if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
+        subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+    else:
+        print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+        sys.exit()
+    if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+        endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+    else:
+        print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+        sys.exit()
+    analyze_url = endpoint + "vision/v3.0/analyze"
 
-if 'COMPUTER_VISION_ENDPOINT' in os.environ:
-    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
-else:
-    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
+    # Set image_path
+    image_path = config_own.IMG_PATH
 
-analyze_url = endpoint + "vision/v3.0/analyze"
+    # Read the image into a byte array
+    image_data = open(image_path, "rb").read()
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key,
+               'Content-Type': 'application/octet-stream'}
+    params = {'visualFeatures': 'Brands,Categories,Description,Color'}
+    response = requests.post(
+        analyze_url, headers=headers, params=params, data=image_data)
+    response.raise_for_status()
 
-# Set image_path
-image_path = config_own.IMG_PATH
+    # Description of the image
+    analysis = response.json() # the JSON return value of the image
+    print(analysis) 
 
-# Read the image into a byte array
-image_data = open(image_path, "rb").read()
-headers = {'Ocp-Apim-Subscription-Key': subscription_key,
-           'Content-Type': 'application/octet-stream'}
-params = {'visualFeatures': 'Brands,Categories,Description,Color'}
-response = requests.post(
-    analyze_url, headers=headers, params=params, data=image_data)
-response.raise_for_status()
+    # split the image name to rename the JSON file
+    base = os.path.basename(image_path)
+    file_name = os.path.splitext(base)[0]
 
-# Description of the image
-analysis = response.json() # the JSON return value of the image
-print(analysis) 
+    # write into the JSON file
+    JSON_dir = config_own.JSON_DIR
+    with open( JSON_dir + file_name + '.json', 'w', encoding='utf-8') as f:
+        json.dump(analysis, f, ensure_ascii=False, indent=4)
 
-# split the image name to rename the JSON file
-base = os.path.basename(image_path)
-file_name = os.path.splitext(base)[0]
+    # set the show title on the image window
+    image_caption = analysis["brands"][0]["name"].capitalize()
+    #print(image_caption)
 
-# write into the JSON file
-JSON_dir = config_own.JSON_DIR
-with open( JSON_dir + file_name + '.json', 'w', encoding='utf-8') as f:
-    json.dump(analysis, f, ensure_ascii=False, indent=4)
+    # Display the image and overlay it with the caption.
+    image = Image.open(BytesIO(image_data))
+    plt.imshow(image)
+    plt.axis("off")
+    _ = plt.title(image_caption, size="x-large", y=-0.1)
+    plt.show()
 
-# set the show title on the image window
-image_caption = analysis["brands"][0]["name"].capitalize()
-#print(image_caption)
-
-# Display the image and overlay it with the caption.
-image = Image.open(BytesIO(image_data))
-plt.imshow(image)
-plt.axis("off")
-_ = plt.title(image_caption, size="x-large", y=-0.1)
-plt.show()
+if __name__ == "__main__":
+    img_processing()
--- computer_vision/size.py
+++ computer_vision/size.py
@@ -10,69 +10,77 @@ import cv2
 import numpy as np
 import config_own
 
-# # cpature image by cv2
-cam = cv2.VideoCapture(0)
-cv2.namedWindow("image capture")
-img_counter = 0
-while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    cv2.imshow("image capture", frame)
 
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frameshot_{}.jpg".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+def shot_cv2():
+	# # cpature image by cv2
+	cam = cv2.VideoCapture(0)
+	cv2.namedWindow("image capture")
+	img_counter = 0
+	while True:
+	    ret, frame = cam.read()
+	    if not ret:
+	        print("failed to grab frame")
+	        break
+	    cv2.imshow("image capture", frame)
 
-cam.release()
-cv2.destroyAllWindows()
+	    k = cv2.waitKey(1)
+	    if k%256 == 27:
+	        # ESC pressed
+	        print("Escape hit, closing...")
+	        break
+	    elif k%256 == 32:
+	        # SPACE pressed
+	        img_name = "opencv_frameshot_{}.jpg".format(img_counter)
+	        cv2.imwrite(img_name, frame)
+	        print("{} written!".format(img_name))
+	        img_counter += 1
+	cam.release()
+	cv2.destroyAllWindows()
 
-# Set the COMPUTER_SUBSCRIPTION_KEY & COMPUTER_VISION_ENDPOINT
-if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
-    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
-else:
-    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
 
-if 'COMPUTER_VISION_ENDPOINT' in os.environ:
-    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
-else:
-    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
 
-analyze_url = endpoint + "vision/v3.0/analyze"
+def img_processing():
+	# Set the COMPUTER_SUBSCRIPTION_KEY & COMPUTER_VISION_ENDPOINT
+	if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
+	    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+	else:
+	    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+	    sys.exit()
 
-# Set image_path
-image_dir = config_own.IMG_DIR
-image_path = image_dir + "opencv_frameshot_0.jpg"
+	if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+	    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+	else:
+	    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+	    sys.exit()
 
-# Read the image into a byte array
-image_data = open(image_path, "rb").read()
-headers = {'Ocp-Apim-Subscription-Key': subscription_key,
-           'Content-Type': 'application/octet-stream'}
-params = {'visualFeatures': 'Brands,Categories,Description,Color'}
-response = requests.post(
-    analyze_url, headers=headers, params=params, data=image_data)
-response.raise_for_status()
+	analyze_url = endpoint + "vision/v3.0/analyze"
 
-# Description of the image
-analysis = response.json() # the JSON return value of the image
-print(analysis) 
+	# Set image_path
+	image_dir = config_own.IMG_DIR
+	image_path = image_dir + "opencv_frameshot_0.jpg"
 
-# split the image name to rename the JSON file
-base = os.path.basename(image_path)
-file_name = os.path.splitext(base)[0]
+	# Read the image into a byte array
+	image_data = open(image_path, "rb").read()
+	headers = {'Ocp-Apim-Subscription-Key': subscription_key,
+	           'Content-Type': 'application/octet-stream'}
+	params = {'visualFeatures': 'Brands,Categories,Description,Color'}
+	response = requests.post(
+	    analyze_url, headers=headers, params=params, data=image_data)
+	response.raise_for_status()
 
-# write into the JSON file
-JSON_dir = config_own.JSON_DIR
-with open( JSON_dir + file_name + '.json', 'w', encoding='utf-8') as f:
-    json.dump(analysis, f, ensure_ascii=False, indent=4)
+	# Description of the image
+	analysis = response.json() # the JSON return value of the image
+	print(analysis) 
+
+	# split the image name to rename the JSON file
+	base = os.path.basename(image_path)
+	file_name = os.path.splitext(base)[0]
+
+	# write into the JSON file
+	JSON_dir = config_own.JSON_DIR
+	with open( JSON_dir + file_name + '.json', 'w', encoding='utf-8') as f:
+	    json.dump(analysis, f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+	shot_cv2()
+	img_processing()
