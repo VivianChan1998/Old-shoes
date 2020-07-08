c30ca45abc9fd8aa7b27b8b9a341b99200ab29fd
FAREAST\v-yhsiao <v-yhsiao@microsoft.com>
2020-07-08 16:37:21 +0800

    CV: 利用讀取.txt檔, 可同時爬多個url, 並將過程紀錄於log.txt

--- computer_vision/img_web_crawler.py
+++ computer_vision/img_web_crawler.py
@@ -13,56 +13,71 @@ import re
 # ...  new_img_tag = img_tag.replace(src, full_src)
 # ...  return new_img_tag
 
-# set up the crawed url
-url = 'https://www.ebay.com/str/usedshoesandmore/Mens-Shoes/_i.html?_pgn=3&_storecat=828750519'
-photolimit = 100
 
-# open a txt file to record the log info
-log_file_dir = config_own.LOG_FILE
-log_file = open(log_file_dir + 'log_file.txt', 'w')
+def web_crawling(url, file_amount, log_file):
+    photolimit = 100
 
-headers = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(url,headers = headers)
-soup = BeautifulSoup(response.content, 'html.parser')
-items = soup.find_all('img')
-print(items)
-items_str = str(items)
-log_file.write(items_str) # write into the log file
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url,headers = headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    items = soup.find_all('img')
+    print(items)
+    items_str = str(items)
+    log_file.write(items_str) # write into the log file
+    log_file.write('-----------------------------------------------\n')
 
-img_storage_dir = config_own.IMG_CRAWLER
-#rex = r'<img.*?data-src="(.*?)">'
-# # # 找找看是否有相對路徑
-# result = re.findall(rex, html)
-# re.sub(rex, fun, html)
+    img_storage_dir = config_own.IMG_CRAWLER
+    #rex = r'<img.*?data-src="(.*?)">'
+    # # # 找找看是否有相對路徑
+    # result = re.findall(rex, html)
+    # re.sub(rex, fun, html)
 
-for index , item in enumerate (soup.find_all('img')): # 抓取所有有<img> tag的東西
+    for index , item in enumerate (soup.find_all('img')): # 抓取所有有<img> tag的東西
 
-    if (item and index < photolimit ):
-		# if there is no "data-src"
-        if item.get('data-src') == None:
-            html = requests.get(item.get('src'))
-            print(item.get('src'))
-			#items_get_str = str(item.get('src'))
-            log_file.write(item.get('src')) # write into the log file
+        if (item and index < photolimit ):
+		    # if there is no "data-src"
+            if item.get('data-src') == None:
+                html = requests.get(item.get('src'))
+                print(item.get('src'))
+    			#items_get_str = str(item.get('src'))
+                log_file.write(item.get('src')) # write into the log file
+                log_file.write('\n')
 
-		# if there is "src"
-        else:
-            html = requests.get(item.get('data-src')) # use 'get' to get photo link path , requests = send request
-            print(item.get('data-src')) 
-            log_file.write(item.get('data-src')) # write into the log file
+    		# if there is "src"
+            else:
+                html = requests.get(item.get('data-src')) # use 'get' to get photo link path , requests = send request
+                print(item.get('data-src')) 
+                log_file.write(item.get('data-src')) # write into the log file
+                log_file.write('\n')
 
+            img_name = img_storage_dir + str(file_amount) + '0' + str(index + 1) + '.jpg'
 
-        img_name = img_storage_dir + str(index + 1) + '.jpg'
+            with open(img_name,'wb') as file: #write in the img by "byte"
+                file.write(html.content)
+                file.flush()
 
-        with open(img_name,'wb') as file: #write in the img by "byte"
-            file.write(html.content)
-            file.flush()
+            file.close() #close file
 
-        file.close() #close file
+            print('%d0%d.jpg processing...' % (file_amount, index + 1))
+            log_file.write('%d0%d.jpg processing...\n' % (file_amount, index + 1))
+            time.sleep(1)
 
-        print('%d.jpg processing...' % (index + 1))
-        log_file.write('%d.jpg processing...' % (index + 1))
-        time.sleep(1)
+    log_file.write('=====================================================\n')
+    print('Done')
 
-log_file.close()
-print('Done')
+if __name__ == "__main__":
+    img_crawler_url_path = config_own.IMG_CRAWLER_URL
+
+	# open a txt file to record the log info
+    log_file_dir = config_own.LOG_FILE
+    log_file = open(log_file_dir + 'log_file.txt', 'w')
+
+    file_amount = 1
+    # import the url file and split lines
+    with open(img_crawler_url_path) as url_file:
+        for line in url_file:
+            url = line.rstrip() # set up the crawed url
+            web_crawling(url,file_amount,log_file)
+            file_amount+=1
+    
+    log_file.close()
