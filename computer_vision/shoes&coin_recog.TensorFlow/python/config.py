87c86571216b61f32a146417d8a7b7ffd63da22c
FAREAST\v-yhsiao <v-yhsiao@microsoft.com>
2020-07-21 17:04:06 +0800

    update the way to find out the position of shoes and coin

---
+++ computer_vision/shoes&coin_recog.TensorFlow/python/config.py
@@ -0,0 +1,3 @@
+MODEL_PATH = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\shoes&coin_recog.Tensorflow\\model.pb"
+LABELS_PATH = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\shoes&coin_recog.Tensorflow\\labels.txt"
+JSON_DIR = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\JSON_file\\"
