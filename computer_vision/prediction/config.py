1502df3c75e0664c3f29a0c51447800683131783
FAREAST\v-yhsiao <v-yhsiao@microsoft.com>
2020-07-24 14:17:22 +0800

    upload new model - to classift the degree of "old" & "new"

---
+++ computer_vision/prediction/config.py
@@ -0,0 +1,3 @@
+MODEL_PATH = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\model\\degree.Tensorflow\\model.pb"
+LABELS_PATH = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\model\\degree.Tensorflow\\labels.txt"
+JSON_DIR = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\JSON_file\\"
--- computer_vision/shoes&coin_recog.TensorFlow/python/config.py
+++
@@ -1,3 +0,0 @@
-MODEL_PATH = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\shoes&coin_recog.Tensorflow\\model.pb"
-LABELS_PATH = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\shoes&coin_recog.Tensorflow\\labels.txt"
-JSON_DIR = "C:\\Users\\v-yhsiao\\Documents\\code\\old_shoes\\computer_vision\\JSON_file\\"
