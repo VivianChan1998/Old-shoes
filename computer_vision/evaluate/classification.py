d1512aa9e7c772e5d184baf05624fae6e870a686
FAREAST\v-yhsiao <v-yhsiao@microsoft.com>
2020-07-27 14:00:43 +0800

    modified classification.py

--- computer_vision/evaluate/classification.py
+++ computer_vision/evaluate/classification.py
@@ -11,17 +11,6 @@ from PIL import Image
 import numpy as np
 import cv2
 
-def set_tf_loglevel(level):
-    if level >= logging.FATAL:
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    if level >= logging.ERROR:
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    if level >= logging.WARNING:
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
-    else:
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
-    logging.getLogger('tensorflow').setLevel(level)
-
 def convert_to_opencv(image):
     # RGB -> BGR conversion is performed as well.
     image = image.convert('RGB')
